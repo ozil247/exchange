71d24149e50e649b233dc4ba3e7baa7bee9364f8
MEL <amaranthblu@yahoo.com>
2024-06-07 19:34:07 +0500

    admin screens

---
+++ assets/admin.js
@@ -0,0 +1,12 @@
+ window.onload = function () {
+   const adminBtn = document.querySelector('#admin-btn');
+   adminBtn.addEventListener('click', function () {
+     const adminSection = document.getElementById('admin-section-flex');
+     const sideBar = document.getElementById('side-bar');
+     adminSection.classList.toggle('active');
+     sideBar.classList.toggle('active');
+     console.log(adminSection);
+
+   });
+ }
+
--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
-// document.write(
-//   unescape("%3Cscript src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' type='text/javascript'%3E%3C/script%3E")
-// );
+//  document.write(
+//  unescape("%3Cscript src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' type='text/javascript'%3E%3C/script%3E")
+//  );
 
 $(document).ready(function () {
   // Add smooth scrolling to all links
@@ -35,3 +35,5 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 });
+
+
